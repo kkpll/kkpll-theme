6f70bc32b3ca9f636d7993e233143610b41ff81a
kkpll <izutsu_daisuke@kokopelli-inc.com>
2019-11-18 18:49:25 +0900

    slideshow

--- functions/admin/callback/slideshow_callback.php
+++ functions/admin/callback/slideshow_callback.php
@@ -13,8 +13,48 @@ class SlideshowCallback {
     }
 
     static function render_field(){
+
+        //delete_option('fnsk_slideshow');
+
+        $slideshows = get_option( 'fnsk_slideshow' );
+
+        $args = array( 'public' => true, '_builtin' => false );
+        $post_types = get_post_types( $args );
+        array_unshift( $post_types, 'post');
+
+        $pts = array();
+
+        foreach( $post_types as $post_type ){
+
+            $obj = get_post_type_object($post_type);
+
+            $terms = array();
+
+            if( $post_type=='post' ){
+                $terms = get_terms(array('post_tag','category'),array('hide_empty'=>false));
+            }
+
+            if( $taxonomies = $obj->taxonomies ){
+
+                $tmp = get_terms( $taxonomies, array( 'hide_empty'=>false ) );
+
+                if($terms){
+                    array_merge( $terms, $tmp );
+                }else{
+                    $terms = $tmp;
+                }
+            }
+
+            $pts[] = array(
+                'slug'  => $post_type,
+                'label' => $obj->labels->singular_name,
+                'terms' => $terms,
+            );
+
+        }
+
         global $twig;
-        echo $twig->render( 'admin/slideshow_field.html' );
+        echo $twig->render( 'admin/slideshow_field.html', array( 'slideshows' => $slideshows, 'post_types' => $pts ));
     }
 
     static function sanitize($input){
